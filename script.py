1ba36f85d6c1822b1cfb8e39ca88b4c395e0fceb
angelina-tsuboi <angelina.t1832@gmail.com>
2020-01-02 14:07:46 -0800

    Added Scraped Data into a CSV File

--- script.py
+++ script.py
@@ -1,9 +1,35 @@
 from bs4 import BeautifulSoup
-import requests
+import re
+import csv
 
 with open('index.html') as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
-print(soup)
+icecream_file = open('icecream_data.csv', 'w')
+csv_writer = csv.writer(icecream_file)
+csv_writer.writerow(['Name', 'Price', 'Rating'])
 
-print(soup.title.text)
+icecreamNamesTag = soup.find_all(attrs={"class": "name"})
+icecreamPricesTag = soup.find_all(attrs={"class": "price"})
+icecreamRatingsTag = soup.find_all(attrs={"class": "rating"})
+
+icecreamNames = []
+icecreamPrice = []
+icecreamRating = []
+
+for name in icecreamNamesTag:
+    icecreamNames.append(name.text)
+
+for price in icecreamPricesTag:
+    priceText = re.search('\d.\d+', price.text).group(0)
+    icecreamPrice.append(float(priceText))
+
+for rating in icecreamRatingsTag:
+    ratingText = re.search('\d+', rating.text).group(0)
+    icecreamRating.append(int(ratingText))
+
+
+for x in range(len(icecreamNames)):
+    csv_writer.writerow([icecreamNames[x], icecreamPrice[x], icecreamRating[x]])
+
+icecream_file.close()
